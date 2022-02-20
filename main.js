8e4b3cad75112c75af9fbb4b856899decb5f7da8
Rasmus Pedersen <rasser31@hotmail.com>
2022-02-20 15:50:44 +0100

    Tilføjet JS elementer
    Der er tilføjet elementer der gør at vi kan ændre headerens elementer til engelsk og dansk.
    Samt er der blevet tilføjet en knap der kan redirecte en til bibliotekets facebook side (Giver også en alert).

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
+function dansk(){
+    document.getElementById('language-changer').src="images/Dansk.png";
+    document.getElementById('language-events').innerHTML = "Arrangementer"
+    document.getElementById('language-books').innerHTML = "Bøger"
+    document.getElementById('language-e-books').innerHTML = "E-bøger & medier"
+    document.getElementById('language-news').innerHTML = "Nyheder"
+    document.getElementById('language-frontpage').innerHTML = "Forside"
+    document.getElementById('language-contact').innerHTML = "Kontakt os "
+    document.getElementById('language-find').innerHTML = "Find os "
+    document.getElementById('language-opening').innerHTML = "Åbningstider"
+    document.getElementById('language-login').innerHTML = "Log ind"
+    document.getElementById('searchbar-text').placeholder = "Skriv f.eks. Harry Potter, krimi eller lign."
+    document.getElementById('search-category').innerHTML = "Søg i alle                                      ▼"
+};
 
+function engelsk(){
+    document.getElementById('language-changer').src="images/Engelsk.png";
+    document.getElementById('language-events').innerHTML = "Events"
+    document.getElementById('language-books').innerHTML = "Books"
+    document.getElementById('language-e-books').innerHTML = "E-books & media"
+    document.getElementById('language-news').innerHTML = "News"
+    document.getElementById('language-frontpage').innerHTML = "Front Page"
+    document.getElementById('language-contact').innerHTML = "Contact us "
+    document.getElementById('language-find').innerHTML = "Find us "
+    document.getElementById('language-opening').innerHTML = "Opening hours"
+    document.getElementById('language-login').innerHTML = "Login"
+    document.getElementById('searchbar-text').placeholder = "Search for example Harry Potter, a genre or etc."
+    document.getElementById('search-category').innerHTML = "Search in all                                 ▼"
+};
+
+
+// https://teamtreehouse.com/community/how-to-add-the-leave-this-page-stay-on-this-page-popup-to-a-website-when-closing
+
+function facebooklink(){
+    window.onbeforeunload = function(e) {
+        return 'Dialog text here. Iffy functionality.';
+}}
